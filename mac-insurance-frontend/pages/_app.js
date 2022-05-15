9e696331422db9714d37cd8fa633f759d0c05390
ale_maci <alessandromaci96@gmail.com>
2022-05-15 15:21:13 +0200

    Adding query to retrieve list of created pools in the market tab + displaying it in the UI

--- mac-insurance-frontend/pages/_app.js
+++ mac-insurance-frontend/pages/_app.js
@@ -1,8 +1,14 @@
 import "../styles/globals.css";
 import "react-toastify/dist/ReactToastify.css";
+import { ApolloProvider } from "@apollo/client";
+import client from "./api/apollo-client";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />;
+    </ApolloProvider>
+  );
 }
 
 export default MyApp;
---
+++ mac-insurance-frontend/pages/api/apollo-client.js
@@ -0,0 +1,8 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://api.studio.thegraph.com/query/27357/mac-insurance/v0.4",
+  cache: new InMemoryCache(),
+});
+
+export default client;
