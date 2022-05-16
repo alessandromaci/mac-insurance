d168febd015224dc342611b7370d541c07c40739
shan5742 <asamshan456@gmail.com>
2022-05-16 10:33:57 +0100

    styling inputs and selects
    (cherry picked from commit 2a997e025d0f5c3f2752586b8f823fbd1d866a75)

--- mac-insurance-frontend/components/Pool.js
+++ mac-insurance-frontend/components/Pool.js
@@ -2,40 +2,101 @@ import s from "../styles/Pool.module.scss"
 import { useState } from "react"
 import Select from "react-select"
 import { Button } from "./Button"
-
-const tokens = [
-  { value: "eth", label: "ETH" },
-  { value: "dai", label: "DAI" },
-]
+import ethLogo from "../public/ethLogo.png"
+import daiLogo from "../public/dai-logo.png"
 
 export const Pool = () => {
   const [selectedToken, setSelectedToken] = useState(null)
+
+  const tokens = [
+    { value: "eth", label: "ETH", img: ethLogo },
+    { value: "dai", label: "DAI", img: daiLogo },
+  ]
+
+  const customStyles = {
+    option: (provided, state) => ({
+      ...provided,
+      borderBottom: "1px dotted pink",
+      color: state.isSelected ? "red" : "blue",
+      padding: 20,
+    }),
+    control: () => ({
+      // none of react-select's styles are passed to <Control />
+      width: "100%",
+    }),
+    singleValue: (provided, state) => {
+      const opacity = state.isDisabled ? 0.5 : 1
+      const transition = "opacity 300ms"
+
+      return { ...provided, opacity, transition }
+    },
+  }
   return (
     <div className={s.container}>
       <div className={s.wrapper}>
         <h3 className={s.tabHeader}>Create Insurance Pool</h3>
         <div className={s.row}>
           <p className={s.rowLabel}>Select Token</p>
-          <Select
-            defaultValue={selectedToken}
-            onChange={setSelectedToken}
-            options={tokens}
-          />
+          <div className={s.rowValue}>
+            <Select
+              theme={(theme) => ({
+                ...theme,
+                borderRadius: 8,
+                colors: {
+                  ...theme.colors,
+                  primary25: "#03cea4",
+                  primary: "#2b2c3b",
+                  neutral0: "#2b2c3b",
+                  neutral80: "#d6edff",
+                },
+              })}
+              defaultValue={selectedToken}
+              onChange={setSelectedToken}
+              options={tokens}
+              formatOptionLabel={(token) => (
+                <div className={s.selectOption}>
+                  <img
+                    className={s.selectImage}
+                    src={token.image}
+                    alt="token-image"
+                  />
+                  <span>{token.label}</span>
+                </div>
+              )}
+              placeholder="Select token..."
+            />
+          </div>
         </div>
         <div className={s.row}>
           <p className={s.rowLabel}>Insurance fee</p>
-          <input type="number" min={0} max={100} />
+          <div className={s.rowValue}>
+            <input
+              className={s.numInput}
+              type="number"
+              min={0}
+              max={100}
+              placeholder="Enter a number between 0-100"
+            />
+          </div>
         </div>
         <div className={s.row}>
           <p className={s.rowLabel}>Price Loss Cover %</p>
           <div className={s.inputWithPreview}>
-            <input type="number" min={0} max={100} />
+            <input
+              className={s.numInput}
+              type="number"
+              min={0}
+              max={100}
+              placeholder="Enter a number between 0-100"
+            />
             <p className={s.lossPreviewText}>preview</p>
           </div>
         </div>
         <div className={s.row}>
           <p className={s.rowLabel}>Validity period</p>
-          <p>some date</p>
+          <div className={s.rowValue}>
+            <p>some date</p>
+          </div>
         </div>
         <div className={s.poolSummary}>
           <h3 className={s.summaryHeading}>Supply Liquidity</h3>
