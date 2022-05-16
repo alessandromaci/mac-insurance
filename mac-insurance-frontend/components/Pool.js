9ea38ca0f8388946c78a9cfe6a20ef5e05a080c0
shan5742 <asamshan456@gmail.com>
2022-05-16 09:36:43 +0100

    cherry-pick

--- mac-insurance-frontend/components/Pool.js
+++ mac-insurance-frontend/components/Pool.js
@@ -1,10 +1,51 @@
 import s from "../styles/Pool.module.scss"
+import { useState } from "react"
+import Select from "react-select"
+import { Button } from "./Button"
+
+const tokens = [
+  { value: "eth", label: "ETH" },
+  { value: "dai", label: "DAI" },
+]
 
 export const Pool = () => {
+  const [selectedToken, setSelectedToken] = useState(null)
   return (
     <div className={s.container}>
       <div className={s.wrapper}>
         <h3 className={s.tabHeader}>Create Insurance Pool</h3>
+        <div className={s.row}>
+          <p className={s.rowLabel}>Select Token</p>
+          <Select
+            defaultValue={selectedToken}
+            onChange={setSelectedToken}
+            options={tokens}
+          />
+        </div>
+        <div className={s.row}>
+          <p className={s.rowLabel}>Insurance fee</p>
+          <input type="number" min={0} max={100} />
+        </div>
+        <div className={s.row}>
+          <p className={s.rowLabel}>Price Loss Cover %</p>
+          <div className={s.inputWithPreview}>
+            <input type="number" min={0} max={100} />
+            <p className={s.lossPreviewText}>preview</p>
+          </div>
+        </div>
+        <div className={s.row}>
+          <p className={s.rowLabel}>Validity period</p>
+          <p>some date</p>
+        </div>
+        <div className={s.poolSummary}>
+          <h3 className={s.summaryHeading}>Supply Liquidity</h3>
+          <p className={s.summaryLiquidity}>ETH: 0.0</p>
+          <Button
+            handleClick={() => console.log("Approve")}
+            className={s.button}
+            buttonText="Approve"
+          />
+        </div>
       </div>
     </div>
   )
