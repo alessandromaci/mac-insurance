78107c54d302101078edf0d6b2c64009ceb0ae6d
ale_maci <alessandromaci96@gmail.com>
2022-05-18 11:16:25 +0200

    adding account variabke in a dynamic way

--- mac-insurance-frontend/components/TabSelector.js
+++ mac-insurance-frontend/components/TabSelector.js
@@ -1,27 +1,31 @@
-import { useState } from "react"
-import { Markets } from "./Markets"
-import { NoWalletDashboard } from "./NoWalletDashboard"
-import { Pool } from "./Pool"
-import { TabButtonContainer } from "./TabButtonContainer"
-import { WalletDashboard } from "./WalletDashboard"
+import { useState } from "react";
+import { Markets } from "./Markets";
+import { NoWalletDashboard } from "./NoWalletDashboard";
+import { Pool } from "./Pool";
+import { TabButtonContainer } from "./TabButtonContainer";
+import { WalletDashboard } from "./WalletDashboard";
 
 export const TabSelector = ({ walletConnected, account }) => {
-  const [selected, setSelected] = useState("Dashboard")
+  const [selected, setSelected] = useState("Dashboard");
 
   const renderTab = () => {
     switch (selected) {
       case "Markets":
-        return <Markets account={account} />
+        return <Markets account={account} />;
       case "Pool":
-        return <Pool />
+        return <Pool />;
       default:
-        return !walletConnected ? <NoWalletDashboard /> : <WalletDashboard />
+        return !walletConnected ? (
+          <NoWalletDashboard />
+        ) : (
+          <WalletDashboard account={account} />
+        );
     }
-  }
+  };
   return (
     <>
       <TabButtonContainer getter={selected} setter={setSelected} />
       {renderTab()}
     </>
-  )
-}
+  );
+};
