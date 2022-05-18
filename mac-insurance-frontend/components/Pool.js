97c2dd9ba637529012c2c71faf75cf0c35f929db
ale_maci <alessandromaci96@gmail.com>
2022-05-18 16:23:44 +0200

    retrieving inputs from the frontend
    (cherry picked from commit c348988e7e8a3333515d2f4bd5c6ff57bd7f9e76)

--- mac-insurance-frontend/components/Pool.js
+++ mac-insurance-frontend/components/Pool.js
@@ -1,44 +1,67 @@
-import s from "../styles/Pool.module.scss"
-import { useEffect, useState } from "react"
-import Select from "react-select"
-import { Button } from "./Button"
-import ethLogo from "../public/ethLogo.png"
-import daiLogo from "../public/dai-logo.png"
-import Image from "next/image"
-import { addDays, format } from "date-fns"
-import { DateRange, DayPicker, useDayRender } from "react-day-picker"
-import "react-day-picker/dist/style.css"
+import s from "../styles/Pool.module.scss";
+import { useEffect, useState } from "react";
+import Select from "react-select";
+import { Button } from "./Button";
+import ethLogo from "../public/ethLogo.png";
+import daiLogo from "../public/dai-logo.png";
+import Image from "next/image";
+import { addDays, format } from "date-fns";
+import { DateRange, DayPicker, useDayRender } from "react-day-picker";
+import "react-day-picker/dist/style.css";
 
 export const Pool = () => {
-  const [selectedToken, setSelectedToken] = useState(null)
-  const [showDatePicker, setShowDatePicker] = useState(false)
-  const [range, setRange] = useState()
-  const [dateRangePicked, setDateRangePicked] = useState(false)
+  const [selectedToken, setSelectedToken] = useState(null);
+  const [showDatePicker, setShowDatePicker] = useState(false);
+  const [range, setRange] = useState();
+  const [dateRangePicked, setDateRangePicked] = useState(false);
+  const [fee, setFee] = useState(null);
+  const [lossCover, setLossCover] = useState(null);
+  const [amount, setAmount] = useState(null);
 
   const tokens = [
-    { value: "eth", label: "ETH", img: ethLogo },
-    { value: "dai", label: "DAI", img: daiLogo },
-  ]
+    {
+      value: "eth",
+      label: "WETH",
+      img: ethLogo,
+      tokenAddress: "0xc778417E063141139Fce010982780140Aa0cD5Ab",
+      priceFeed: "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
+    },
+    {
+      value: "dai",
+      label: "DAI",
+      img: daiLogo,
+      contractAddress: "0xc7AD46e0b8a400Bb3C915120d284AafbA8fc4735",
+      priceFeed: "0x2bA49Aaa16E6afD2a993473cfB70Fa8559B523cF",
+    },
+  ];
 
-  let footer = <p>Please pick the first day.</p>
+  let footer = <p>Please pick the first day.</p>;
   if (range?.from) {
     if (!range.to) {
-      footer = <p>{format(range.from, "PPP")}</p>
+      footer = <p>{format(range.from, "PPP")}</p>;
     } else if (range.to) {
       footer = (
         <p>
           {format(range.from, "PPP")}–{format(range.to, "PPP")}
         </p>
-      )
+      );
     }
   }
 
+  // time variables to calculate the contract inputs in second differences
+  const t0 = new Date();
+  const t1 = new Date(range?.from);
+  const t2 = new Date(range?.to);
+
+  const startDate = ((t1.getTime() - t0.getTime()) / 1000).toFixed(0); //_startDateFromDeployInSeconds
+  const endDate = ((t2.getTime() - t0.getTime()) / 1000).toFixed(0); //_endDateFromDeployInSeconds
+
   useEffect(() => {
     if (range && range.from && range.to) {
-      setShowDatePicker(false)
-      setDateRangePicked(true)
+      setShowDatePicker(false);
+      setDateRangePicked(true);
     }
-  }, [range])
+  }, [range]);
   return (
     <div className={s.container}>
       <div className={s.wrapper}>
@@ -85,6 +108,7 @@ export const Pool = () => {
               min={0}
               max={100}
               placeholder="0%-100%..."
+              onChange={(e) => setFee(e.target.value)}
             />
           </div>
         </div>
@@ -97,6 +121,7 @@ export const Pool = () => {
               min={0}
               max={100}
               placeholder="0%-100%..."
+              onChange={(e) => setLossCover(e.target.value)}
             />
             <p className={s.lossPreviewText}>{`[<$2357.10]`}</p>
           </div>
@@ -127,7 +152,7 @@ export const Pool = () => {
               <div className={s.dateSelectedContainer}>
                 {" "}
                 <p className={s.dateRange}>
-                  {format(range.from, "PPP")}–{format(range.to, "PPP")}
+                  {format(range.from, "PPP")} – {format(range.to, "PPP")}
                 </p>
                 <button
                   onClick={() => setShowDatePicker(true)}
@@ -149,6 +174,7 @@ export const Pool = () => {
               min={0}
               defaultValue={0.0}
               step={0.1}
+              onChange={(e) => setAmount(e.target.value)}
             />
           </div>
 
@@ -160,5 +186,5 @@ export const Pool = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
