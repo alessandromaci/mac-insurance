e96ba6a26fb0d9188a333728a7789d20a97b64a7
ale_maci <alessandromaci96@gmail.com>
2022-05-19 10:06:14 +0200

    adding preview fee function

--- mac-insurance-frontend/components/modals/RequestModal.js
+++ mac-insurance-frontend/components/modals/RequestModal.js
@@ -9,25 +9,21 @@ const alchemyKey = process.env.NEXT_PUBLIC_ALCHEMY_KEY;
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
 const web3 = createAlchemyWeb3(alchemyKey);
 const macContractAddress = MacInsurance.address;
-const macContractInstance = new web3.eth.Contract(
-  MacInsurance.abi,
-  macContractAddress
-);
 
 export const RequestModal = ({ item, pool, show, onClose, account }) => {
   const [isBrowser, setIsBrowser] = useState(false);
   const [amount, setAmount] = useState(0);
+  const [fee, setFee] = useState(0);
+
+  const macContractInstance = new web3.eth.Contract(
+    MacInsurance.abi,
+    macContractAddress
+  );
 
   const tokenContractInstance = new web3.eth.Contract(
     ERC20.abi,
     pool?.tokenAddress
   );
-
-  console.log(account);
-
-  const handleAmountChange = (e) => {
-    setAmount(e.target.value);
-  };
 
   const requestInsurance = async () => {
     // Step 1: Call the ERC20 contract to approve the transfer of tokens
@@ -56,9 +52,28 @@ export const RequestModal = ({ item, pool, show, onClose, account }) => {
     }
   };
 
+  const getFee = async () => {
+    try {
+      const fee = await macContractInstance.methods
+        .getUserFee(pool?.poolId, ethers.utils.parseEther(amount))
+        .call();
+      if (fee) {
+        setFee(fee);
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   useEffect(() => {
     setIsBrowser(true);
   }, []);
+
+  useEffect(() => {
+    if (amount > 0 && pool) {
+      getFee();
+    }
+  }, [amount]);
 
   const modalContent = show ? (
     <div className={s.modalOverlay}>
@@ -71,10 +86,13 @@ export const RequestModal = ({ item, pool, show, onClose, account }) => {
             className={s.priceValue}
             type="number"
             placeholder="0"
-            onChange={handleAmountChange}
+            onChange={(e) => setAmount(e.target.value)}
           />
         </div>
-        <p>[Insurance Fee: {pool.feePercentage}%]</p>
+        <p>
+          [Preview Insurance Fee: {fee ? ethers.utils.formatEther(fee) : "na"}{" "}
+          {item.name}]
+        </p>
         <div className={s.buttonContainer}>
           <button className={s.modalButton} onClick={requestInsurance}>
             Request
