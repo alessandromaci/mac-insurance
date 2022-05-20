cb6ded6d9c5a5e208e454ad6d14de740abf49b93
ale_maci <alessandromaci96@gmail.com>
2022-05-20 13:12:28 +0200

    adding a new insurance request to demo the reimbursement

--- mac-insurance-frontend/components/WalletDashboard.js
+++ mac-insurance-frontend/components/WalletDashboard.js
@@ -9,6 +9,7 @@ import { gql, useQuery } from "@apollo/client";
 import { ethers } from "ethers";
 import { useRetrieveTokenData } from "../hooks/useRetrieveTokenData";
 import MacInsurance from "../abis/MacInsuranceMain.json";
+import TestMacInsurance from "../abis/Test_MacInsuranceMain.json";
 
 const GET_PROFILE_POOLS = gql`
   query ($insuranceRequester: Bytes!) {
@@ -31,6 +32,11 @@ const macContractAddress = MacInsurance.address;
 const macContractInstance = new web3.eth.Contract(
   MacInsurance.abi,
   macContractAddress
+);
+
+const testMacContractInstance = new web3.eth.Contract(
+  TestMacInsurance.abi,
+  TestMacInsurance.address
 );
 
 export const WalletDashboard = ({ account }) => {
@@ -59,6 +65,39 @@ export const WalletDashboard = ({ account }) => {
 
   const retrieveTokenData = (tokenAddress) =>
     useRetrieveTokenData(tokenAddress);
+  // small hack to show how the reimbursement will work. I have created a separate pool list that will interact with test contract only
+  const testPools = [
+    {
+      createdAtTimestamp: "1652973323",
+      feeAmount: "5000000000000000000",
+      insuranceLiquidityRequest: "100000000000000000000",
+      poolId: 1,
+      tokenAddress: "0xc7ad46e0b8a400bb3c915120d284aafba8fc4735",
+      __typename: "InsuranceRequestEntity",
+    },
+  ];
+
+  const retrieveTokenData = (tokenAddress) => {
+    const tokenData = {
+      name: "",
+      logo: "",
+    };
+    const checkSumAddress = ethers.utils.getAddress(tokenAddress);
+    switch (checkSumAddress) {
+      case "0xc7AD46e0b8a400Bb3C915120d284AafbA8fc4735":
+        tokenData.name = "DAI";
+        tokenData.logo = daiLogo;
+        break;
+      case "0xc778417E063141139Fce010982780140Aa0cD5Ab":
+        tokenData.name = "WETH";
+        tokenData.logo = ethLogo;
+        break;
+      default:
+        break;
+    }
+    return tokenData;
+  };
+>>>>>>> 5778e86 (adding a separate test flow to show how reimbursement works)
 
   const reimburse = async (poolId) => {
     const transactionParams = {
@@ -66,6 +105,34 @@ export const WalletDashboard = ({ account }) => {
       from: account,
       to: macContractAddress,
       data: macContractInstance.methods
+        .requestReimbursement(poolId)
+        .encodeABI(),
+    };
+
+    try {
+      await web3.eth.sendTransaction(transactionParams);
+      //Do reimburse stuff
+      toast.success("You received your reimbursement!", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+    } catch (err) {
+      console.log("err: ", err);
+    }
+  };
+
+  const testReimburse = async (poolId) => {
+    const transactionParams = {
+      // Hardcoded address for testing
+      from: account,
+      to: TestMacInsurance.address,
+      data: testMacContractInstance.methods
         .requestReimbursement(poolId)
         .encodeABI(),
     };
@@ -137,6 +204,37 @@ export const WalletDashboard = ({ account }) => {
                   >
                     Details
                   </button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className={s.dataContainer}>
+          {testPools?.map((item, index) => (
+            <div key={index}>
+              <div className={s.tableRow}>
+                <div className={s.data}>
+                  <Image
+                    src={retrieveTokenData(item.tokenAddress).logo}
+                    width={30}
+                    height={30}
+                  />
+                  <p>TEST {retrieveTokenData(item.tokenAddress).name}</p>
+                </div>
+                <p className={s.data}>
+                  {item.insuranceLiquidityRequest / 10 ** 18}
+                </p>
+                <p className={s.data}>{item.feeAmount / 10 ** 18}</p>
+                <div className={s.data}>
+                  <button
+                    onClick={() => testReimburse(item.poolId)}
+                    className={s.dataButton}
+                  >
+                    Reimburse
+                  </button>
+                </div>
+                <div className={s.data}>
+                  <button className={s.dataButton}>Details</button>
                 </div>
               </div>
             </div>
